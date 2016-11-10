ba21a3f880b62ff3a290d43f834e425fa9022d51
Ben <ben.k.richter@gmail.com>
2016-11-10 22:44:21 +0100

    Move js to src directory Use compiled file

--- README.md
+++ README.md
@@ -5,5 +5,4 @@
 - **npm run ionic_install** (installs *yarn* and gulp and ionic and stuff global)
 - **npm/yarn install**
 - **gulp install** (check for git and install bower)
-
 - **ionic serve** (run the thing)
---
+++ js/app.js
@@ -0,0 +1,63 @@
+/**
+ * Angular base module
+ *
+ * app - application name in <body>
+ * array - required modules
+ */
+angular.module('app', ['ionic', 'LocalStorageModule', 'controllers', 'services-factory', 'directives'])
+
+    .run(function ($ionicPlatform) {
+        "use strict";
+
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    /**
+     *  Configuration set namespace for local storage
+     *  Todo: use storage: https://scotch.io/tutorials/create-your-first-mobile-app-with-angularjs-and-ionic#building-out-the-real-stuff
+     */
+    .config(function (localStorageServiceProvider) {
+        "use strict";
+
+        localStorageServiceProvider
+            .setPrefix('stackDO');
+    })
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+        "use strict";
+
+        // AngularUI Router with states
+        // https://github.com/angular-ui/ui-router
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
+            .state('app.search', {
+                url: '/search',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/search.html',
+                        controller: 'SearchCtrl'
+                    }
+                }
+            });
+
+        $urlRouterProvider.otherwise('/');
+
+    });
+
